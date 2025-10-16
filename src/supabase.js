61e9ce1dcf4f766e5078ae53a6d2f4bea2ec05a0
DevAniela <dismal.damsel@gmail.com>
2025-10-17 00:38:12 +0300

    Reconnect project to new Supabase database; add .gitignore to exclude env and node_modules

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://epfohmcrglauvkdyscph.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwZm9obWNyZ2xhdXZrZHlzY3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5OTcyMzMsImV4cCI6MjAzMjU3MzIzM30.T0BErTRR8xQAt3MOkDfLrfbsqMPW7KWcLqPM6_hR0K8";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
